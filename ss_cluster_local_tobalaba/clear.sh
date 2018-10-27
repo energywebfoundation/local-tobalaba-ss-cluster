14a0184534a360af6b9b496ae88d3f58d39b2676
ngyam <ngyam@github.com>
2018-10-27 02:07:19 +0200

    feat(local-cluster): first draft ready
    - admin keyfile
    - config files done
    - scripts modified with logs

--- ss_cluster_local_tobalaba/clear.sh
+++ ss_cluster_local_tobalaba/clear.sh
@@ -2,3 +2,10 @@
 ../parity --config ./conf_alice.toml db kill &
 ../parity --config ./conf_bob.toml db kill &
 ../parity --config ./conf_charlie.toml db kill
+
+# only works if script is executed from this file's location
+FILEDIR=`dirname "$0"`
+
+rm -rf $FILEDIR/db.alice/secretstore/db
+rm -rf $FILEDIR/db.bob/secretstore/db
+rm -rf $FILEDIR/db.charlie/secretstore/db
--- ss_cluster_local_tobalaba/start.sh
+++ ss_cluster_local_tobalaba/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LOGLEVEL=${1:-info}
-RUST_LOG=$LOGLEVEL ../parity --config ./conf_alice.toml &
-RUST_LOG=$LOGLEVEL ../parity --config ./conf_bob.toml &
-RUST_LOG=$LOGLEVEL ../parity --config ./conf_charlie.toml
+RUST_LOG=$LOGLEVEL,secretstore=trace,secretstore_net=trace ../parity --config ./conf_alice.toml &
+RUST_LOG=$LOGLEVEL,secretstore=trace,secretstore_net=trace ../parity --config ./conf_bob.toml &
+RUST_LOG=$LOGLEVEL,secretstore=trace,secretstore_net=trace ../parity --config ./conf_charlie.toml
